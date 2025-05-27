ff00f182526a6dbeed2712463a0652de1bf5cd98
MrTipson <mr@tipson.xyz>
2025-05-27 22:58:45 +0200

    Discord: fix transparency

--- modules/graphical/discord.nix
+++ modules/graphical/discord.nix
@@ -4,29 +4,30 @@
     sha256 = "sha256:1lajf7h8xyhrj0zpsqhcq88xcqf5hs80cyqk3rm7vdrsqdghhgjv";
   };
   style = builtins.toFile "discord-custom.css" (with config.lib.stylix.colors.withHashtag; /*css*/ ''
+    @import url("https://mrtipson.github.io/DiscordCSS/css/base.css");
+    @import url("https://mrtipson.github.io/DiscordCSS/css/base16.css");
     @import url("https://mrtipson.github.io/DiscordCSS/css/server-bar.css");
     @import url("https://mrtipson.github.io/DiscordCSS/css/server-folders.css");
     @import url("https://mrtipson.github.io/DiscordCSS/css/channel-sidebar-utils.css");
-    @import url("https://mrtipson.github.io/DiscordCSS/css/base.css");
-    @import url("https://mrtipson.github.io/DiscordCSS/css/base16.css");
+    @import url("https://mrtipson.github.io/DiscordCSS/css/candy-land.css");
 
-    .theme-light, .theme-dark {
-      --base00: ${base00};
-      --base01: ${base01};
-      --base02: ${base02};
-      --base03: ${base03};
-      --base04: ${base04};
-      --base05: ${base05};
-      --base06: ${base06};
-      --base07: ${base07};
-      --base08: ${base08};
-      --base09: ${base09};
-      --base0A: ${base0A};
-      --base0B: ${base0B};
-      --base0C: ${base0C};
-      --base0D: ${base0D};
-      --base0E: ${base0E};
-      --base0F: ${base0F};
+    .theme-light.theme-light, .theme-dark.theme-dark {
+      --base00: ${base00}fe;
+      --base01: ${base01}fe;
+      --base02: ${base02}fe;
+      --base03: ${base03}fe;
+      --base04: ${base04}fe;
+      --base05: ${base05}fe;
+      --base06: ${base06}fe;
+      --base07: ${base07}fe;
+      --base08: ${base08}fe;
+      --base09: ${base09}fe;
+      --base0A: ${base0A}fe;
+      --base0B: ${base0B}fe;
+      --base0C: ${base0C}fe;
+      --base0D: ${base0D}fe;
+      --base0E: ${base0E}fe;
+      --base0F: ${base0F}fe;
 
       --color-main: rgb(68, 152, 255);
       --window-opacity: ${builtins.toString config.stylix.opacity.applications};
@@ -37,6 +38,6 @@ in
   home.packages = [ pkgs.discord ];
 
   home.activation.activateDiscord = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    run cat ${inject} | sh -s -- -c "${style}"
+    run cat ${inject} | sh -s -- -c "${style}" -t
   '';
 }
