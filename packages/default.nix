eb7f4e2ecf877b15d58cb6c586208d55c59d2a48
MrTipson <mr@tipson.xyz>
2025-06-15 22:09:26 +0200

    tpkgs: remove desktop/specialisation select scripts

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,4 @@
 { callPackage, ... }: {
   tofi-recursive-file = callPackage ./tofi-recursive-file.nix {};
   tofi-nix-run = callPackage ./tofi-nix-run.nix {};
-  user-desktop-select = callPackage ./user-desktop-select.nix {};
-  user-specialisation-select = callPackage ./user-specialisation-select.nix {};
 }
