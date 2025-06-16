b853b035b4e8ce9ada491429083c5179e97dcdd7
MrTipson <mr@tipson.xyz>
2025-06-16 18:32:23 +0200

    Vscode: setup language server

--- modules/graphical/vscode.nix
+++ modules/graphical/vscode.nix
@@ -14,6 +14,9 @@
         # manually: https://open-vsx.org/extension/jeanp413/open-remote-ssh
       ];
       userSettings = {
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "${pkgs.nil}/bin/nil";
+        "nix.serverSettings.nil.formatting.command" = [ "${pkgs.nixfmt-rfc-style}/bin/nixfmt" ];
         "clangd.path" = "${pkgs.llvmPackages_19.clang-tools}/bin/clangd";
         "remote.SSH.connectTimeout" = 120;
         "remote.SSH.useLocalServer" = false;
