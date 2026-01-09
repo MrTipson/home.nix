bf3a957f245181ece8051a3e120493ec11315ee3
MrTipson <mr@tipson.xyz>
2026-01-10 00:08:25 +0100

    Update vscode module

--- modules/graphical/vscode.nix
+++ modules/graphical/vscode.nix
@@ -26,6 +26,7 @@
         bradlc.vscode-tailwindcss
         ms-vscode.live-server
         llvm-vs-code-extensions.vscode-clangd
+        ms-python.python
         # denoland.vscode-deno
         # manually: https://open-vsx.org/extension/jeanp413/open-remote-ssh
       ];
@@ -33,14 +34,17 @@
         "nix.enableLanguageServer" = true;
         "nix.serverPath" = "${pkgs.nil}/bin/nil";
         "nix.serverSettings.nil.formatting.command" = [ "${pkgs.nixfmt-rfc-style}/bin/nixfmt" ];
-        "clangd.path" = "${pkgs.llvmPackages_19.clang-tools}/bin/clangd";
+        "clangd.path" = "${pkgs.clang-tools}/bin/clangd"; # cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .
         "remote.SSH.connectTimeout" = 120;
         "remote.SSH.useLocalServer" = false;
         "terminal.integrated.inheritEnv" = false;
         "files.associations" = {
           "*.css" = "tailwindcss";
         };
+        "diffEditor.ignoreTrimWhitespace" = false;
         "editor.formatOnSave" = true;
+        "chat.disableAIFeatures" = true;
+        "workbench.secondarySideBar.defaultVisibility" = "hidden";
       };
     };
   };
